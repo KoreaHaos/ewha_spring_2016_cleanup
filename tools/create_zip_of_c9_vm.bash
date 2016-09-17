f6cbbf2523e4f61afb45b9972329d1080da17514
KoreaHaos <benjaminhaos@ewha.ac.kr>
2016-09-17 20:41:38 +0000

    Modify tool to work outside workspace.

--- tools/create_zip_of_c9_vm.bash
+++ tools/create_zip_of_c9_vm.bash
@@ -1,8 +1,12 @@
 echo "hello!"
 zip_file_name="$C9_PROJECT.zip"
+cleanup_directory="$C9_PROJECT""_CLEAN_UP_REPO"
 echo "$zip_file_name"
+echo "$cleanup_directory"
 echo "$0"
 cd $GOPATH
 cd ..
-zip -r "$GOPATH/$zip_file_name" "workspace"*
+git config --global credential.helper cache
+#git clone https://github.com/KoreaHaos/ewha_spring_2016_cleanup.git "$C9_PROJECT_CLEAN_UP_DIR"
+#zip -r "$GOPATH/$zip_file_name" "workspace"*
 #zip -r "$GOPATH/$zip_file_name" "workspace"* -x *ewha_spring_2016_cleanup*
